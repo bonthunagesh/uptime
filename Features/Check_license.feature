15f685de17cdffa00fb1b4585f64971c0ff45696
BonthuNageshRao <bonthu.nageshrao@gmail.com>
2025-04-21 22:11:37 +0530

    3 Test cases done - Working fine(Assertion needs to updated for UI and License)

---
+++ Features/Check_license.feature
@@ -0,0 +1,7 @@
+Feature: License information
+
+  Scenario: Verify that user has Valid License
+    Given : user is logged in
+    When : user click on Infra Tab
+    And : User click on License Info
+    Then : User should be validate for Valid License
--- Steps/login.py
+++ Steps/login.py
@@ -4,34 +4,33 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+@given(u': user is logged')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+    time.sleep(2)
+
+    context.driver.get("http://localhost:9999/index.php?loggedout")
+    time.sleep(4)
 
 
 @when(u': user enters username and password')
 def step_impl(context):
+
     context.username = context.driver.find_element(By.ID, "username")
     context.username.send_keys("nagesh")
-
-    time.sleep(2)
-
-    context.password = context.driver.find_element(By.ID,"password")
+    context.password = context.driver.find_element(By.ID, "password")
     context.password.send_keys("Alpha122@")
     time.sleep(2)
-
-    try:
-        ok_button = context.driver.find_element(By.XPATH, "//button[text()='OK']")
-        ok_button.click()
-        print("Popup closed successfully.")
-    except Exception as e:
-        print(f"Error closing popup: {e}")
-
+    context.Login = context.driver.find_element(By.ID, "loginButton")
+    context.Login.click()
+    time.sleep(2)
 
 
 
 @then(u': user should be able to Log in to the application')
 def step_impl(context):
-    time.sleep(2)
 
-    assert "My Portal" in context.title, f"Expected 'Dashboard' in title, but got: {context.title}"
+    assert "My Portal" in context.driver.title, f"Expected 'Dashboard' in title, but got: {context.driver.title}"
 
     context.driver.quit()
 
